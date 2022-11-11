6a88cf094c336fdcc96fb6a246e84da21d225148
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2022-11-11 16:55:02 -0500

    restoring old way of starting listeners

--- src/msg_handler/asgs_run_props_msg_svc.py
+++ src/msg_handler/asgs_run_props_msg_svc.py
@@ -9,6 +9,9 @@
 
     Authors: Lisa Stillwell, Phil Owen @RENCI.org
 """
+import os
+import pika
+
 from src.common.logger import LoggingUtil
 from src.common.asgs_queue_callback import AsgsQueueCallback
 
@@ -26,15 +29,30 @@ def run():
     logger = LoggingUtil.init_logging("APSVIZ.APSViz-Msg-Handler.asgs_run_props_msg_svc", level=log_level, line_format='medium',
                                       log_file_path=log_path)
 
-    logger.info("Initializing asgs_run_props_msg_svc handler.")
-
     try:
+        logger.info("Initializing asgs_run_props_msg_svc handler.")
+
+        # set up AMQP credentials and connect to asgs queue
+        credentials = pika.PlainCredentials(os.environ.get("RABBITMQ_USER"), os.environ.get("RABBITMQ_PW"))
+
+        parameters = pika.ConnectionParameters(os.environ.get("RABBITMQ_HOST"), 5672, '/', credentials, socket_timeout=2)
+
+        connection = pika.BlockingConnection(parameters)
+
+        channel = connection.channel()
+
+        channel.queue_declare(queue='asgs_props')
+
+        logger.info("asgs_run_props_msg_svc channel and queue declared.")
+
         # get an instance to the callback handler
         queue_callback_inst = AsgsQueueCallback(_logger=logger)
 
-        # start consuming the messages
-        queue_callback_inst.start_consuming('asgs_props', queue_callback_inst.asgs_run_props_callback)
+        channel.basic_consume('asgs_props', queue_callback_inst.asgs_run_props_callback, auto_ack=True)
 
+        logger.info('asgs_run_props_msg_svc configured and waiting for messages...')
+
+        channel.start_consuming()
     except Exception:
         logger.exception("FAILURE - Problems initiating asgs_run_props_msg_svc")
 
--- src/msg_handler/ecflow_run_props_msg_svc.py
+++ src/msg_handler/ecflow_run_props_msg_svc.py
@@ -9,6 +9,8 @@
 
     Authors: Lisa Stillwell, Phil Owen @RENCI.org
 """
+import os
+import pika
 
 from src.common.logger import LoggingUtil
 from src.common.asgs_queue_callback import AsgsQueueCallback
@@ -27,15 +29,30 @@ def run():
     logger = LoggingUtil.init_logging("APSVIZ.APSViz-Msg-Handler.ecflow_run_props_msg_svc", level=log_level, line_format='medium',
                                       log_file_path=log_path)
 
-    logger.info("Initializing ecflow_run_props_msg_svc handler.")
-
     try:
+        logger.info("Initializing ecflow_run_props_msg_svc handler.")
+
+        # set up AMQP credentials and connect to asgs queue
+        credentials = pika.PlainCredentials(os.environ.get("RABBITMQ_USER"), os.environ.get("RABBITMQ_PW"))
+
+        parameters = pika.ConnectionParameters(os.environ.get("RABBITMQ_HOST"), 5672, '/', credentials, socket_timeout=2)
+
+        connection = pika.BlockingConnection(parameters)
+
+        channel = connection.channel()
+
+        channel.queue_declare(queue='rp_queue')
+
+        logger.info("ecflow_run_props_msg_svc channel and queue declared.")
+
         # get an instance to the callback handler
         queue_callback_inst = AsgsQueueCallback(_logger=logger)
 
-        # start consuming the messages
-        queue_callback_inst.start_consuming('rp_queue', queue_callback_inst.ecflow_run_props_callback)
+        channel.basic_consume('rp_queue', queue_callback_inst.ecflow_run_props_callback, auto_ack=True)
 
+        logger.info('ecflow_run_props_msg_svc configured and waiting for messages...')
+
+        channel.start_consuming()
     except Exception:
         logger.exception("FAILURE - Problems initiating ecflow_run_props_msg_svc.")
 
