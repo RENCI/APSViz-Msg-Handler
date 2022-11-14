226db835c79779f62c5c31642e489f3da3a0fba6
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2022-11-14 14:00:24 -0500

    parameterizing test inputs

--- test/send_test_msgs.py
+++ test/send_test_msgs.py
@@ -10,13 +10,16 @@
     Author: Phil Owen, RENCI.org
 """
 import os
+import sys
 import time
+import argparse
 import pika
 
 
-def queue_message(message):
+def queue_message(queue: str, message: str):
     """
 
+    :param queue:
     :param message:
     :return:
     """
@@ -29,22 +32,42 @@ def queue_message(message):
     channel = connection.channel()
 
     # channel.queue_declare(queue="asgs_queue")
-    channel.queue_declare(queue="asgs_props")
+    channel.queue_declare(queue=queue)
 
     # channel.basic_publish(exchange='',routing_key='asgs_queue',body=message)
-    channel.basic_publish(exchange='', routing_key='asgs_props', body=message)
+    channel.basic_publish(exchange='', routing_key=queue, body=message)
     connection.close()
 
 
-# with open('ASGS_status_msgs.json', 'r', encoding='utf-8') as f:
-# with open('run_property_example1.json', 'r', encoding='utf-8') as f:
-# with open('run_property_example2.json', 'r', encoding='utf-8') as f:
-# with open('run_property_example3.json', 'r', encoding='utf-8') as f:
+if __name__ == '__main__':
+    # main entry point for the rule run.
 
-# open messages file
-with open('run_property_example3.json', 'r', encoding='utf-8') as f:
-    for line in f:
-        msg_obj = line
-        queue_message(msg_obj)
-        print(msg_obj)
-        time.sleep(2)
+    # create a command line parser
+    parser = argparse.ArgumentParser(description='help', formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # assign the expected input args
+    parser.add_argument('-f', '--filename', help='Input file name that contains test queue data')
+    parser.add_argument('-q', '--queue', help='the name of the queue that will be posted to.')
+    parser.add_argument('-p', '--pause', help='the amount of time to pause (in seconds) between queue submissions.', default=2)
+
+    # parse the command line
+    args = parser.parse_args()
+
+    # open messages file
+    with open(args.filename, 'r', encoding='utf-8') as f:
+        # submit each line in the file to the queue
+        for line in f:
+            # the data is in json/text format
+            msg_obj = line
+
+            # send the msg to the queue specified
+            queue_message(args.queue, msg_obj)
+
+            # something for the user
+            print(msg_obj)
+
+            # pause between each queue submission
+            time.sleep(int(args.pause))
+
+    # exit with pass/fail
+    sys.exit(0)
