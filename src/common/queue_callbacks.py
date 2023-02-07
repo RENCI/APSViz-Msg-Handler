31461ff1d9a295ae50483d1359a8fb3fb51417d5
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2023-02-07 09:59:39 -0500

    adding explicit queue msg and DB handler for ecflow run props, tidying up comments, log messages, class/method names

--- src/common/queue_callbacks.py
+++ src/common/queue_callbacks.py
@@ -19,7 +19,7 @@ from src.common.queue_utils import QueueUtils
 from src.common.asgs_constants import AsgsConstants
 
 
-class AsgsQueueCallback:
+class QueueCallbacks:
     """
     callback methods to handle posts to the RabbitMQ
     """
@@ -41,9 +41,9 @@ class AsgsQueueCallback:
             log_level, log_path = LoggingUtil.prep_for_logging()
 
             # create a logger
-            self.logger = LoggingUtil.init_logging("APSVIZ.Archiver.ASGSQueueCallback", level=log_level, line_format='medium', log_file_path=log_path)
+            self.logger = LoggingUtil.init_logging("APSVIZ.Msg-Handler.QueueCallbacks", level=log_level, line_format='medium', log_file_path=log_path)
 
-        self.logger.info("Initializing ASGSQueueCallback for queue %s", _queue_name)
+        self.logger.info("Initializing QueueCallback for queue %s", _queue_name)
 
         # define and init the object used to handle ASGS constant conversions
         self.asgs_constants = AsgsConstants(_logger=self.logger)
@@ -57,11 +57,11 @@ class AsgsQueueCallback:
         # create the general queue utilities class
         self.general_utils = GeneralUtils(_logger=self.logger)
 
-        self.logger.info("ASGSQueueCallback initialization for queue %s complete.", _queue_name)
+        self.logger.info("QueueCallback initialization for queue %s complete.", _queue_name)
 
     def asgs_status_msg_callback(self, channel, method, properties, body):
         """
-        main worker that operates on the incoming ASGS status messages from the queue
+        main worker that operates on the incoming ASGS status message queue
 
         :param channel:
         :param method:
@@ -85,11 +85,11 @@ class AsgsQueueCallback:
             # get the 3vent type if from the event name in the message
             event_type_id, event_name = self.asgs_constants.get_lu_id_from_msg(msg_obj, "event_type", "event_type")
 
-            # get the 3vent type if from the event name in the message
+            # get the event type if from the event name in the message
             state_id, state_name = self.asgs_constants.get_lu_id_from_msg(msg_obj, "state", "state_type")
 
             # get the event advisory data
-            advisory_id = msg_obj.get("advisory_number", "N/A") if (msg_obj.get("advisory_number", "N/A") != "") else "N/A"
+            advisory_id = msg_obj.get("advisory", "N/A") if (msg_obj.get("advisory", "N/A") != "") else "N/A"
 
             # did we get everything needed
             if site_id[0] >= 0 and event_type_id >= 0 and state_id >= 0 and advisory_id != 'N/A':
@@ -157,7 +157,7 @@ class AsgsQueueCallback:
 
     def asgs_run_props_callback(self, channel, method, properties, body):
         """
-        The callback function for the run properties queue
+        The callback function for the ASGS run properties message queue
 
         :param channel:
         :param method:
@@ -182,6 +182,7 @@ class AsgsQueueCallback:
             # get the site id from the name in the message
             site_id = self.asgs_constants.get_lu_id_from_msg(msg_obj, "physical_location", "site")
 
+            # insure we have a legit location
             if site_id is None or site_id[0] < 0:
                 err_msg = f'{context}: ERROR Unknown physical location {msg_obj.get("physical_location", "")}, Ignoring message'
 
@@ -212,7 +213,7 @@ class AsgsQueueCallback:
 
                         if param_list is not None:
                             # insert the records
-                            ret_msg = self.asgs_db.insert_config_items(instance_id, param_list)
+                            ret_msg = self.asgs_db.insert_asgs_config_items(instance_id, param_list, 'ASGS')
 
                             if ret_msg is not None:
                                 err_msg = f'{context}: Error - DB insert for run properties message failed: {ret_msg}, ignoring message.'
@@ -247,9 +248,17 @@ class AsgsQueueCallback:
             # send a message to slack
             self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
 
-    def ecflow_rp_run_props_callback(self, channel, method, properties, body):
+    def ecflow_run_props_callback(self, channel, method, properties, body):
         """
-        The callback function for the ecflow_rp run properties queue
+        The callback function for the ecflow run properties message queue
+
+        Note - the supervisor is expecting the following mappings:
+        'adcirc.gridname' = suite.adcirc.grid
+        'instancename' = suite.instance_name
+        'supervisor_job_status' = 'new'
+        'forcing.stormname' = forcing.stormname
+        'workflow_type' = 'ecflow'
+        '%downloadurl%' = output.downloadurl
 
         :param channel:
         :param method:
@@ -257,17 +266,97 @@ class AsgsQueueCallback:
         :param body:
         :return:
         """
+        # init the return message
+        ret_msg = None
+
         self.logger.info("Received ECFlow_rp run props msg. Body is %s bytes.", len(body))
-        self.logger.debug("Received ECFlow_rp run props msg. channel: %s, method: %s, properties: %s.", channel, method, properties)
+        self.logger.debug("Received ECFlow_rp run props msg. channel: %s, method: %s, properties: %s", channel, method, properties)
 
-        # if there is a relay host set send the message over there
-        if os.environ.get("RELAY_RABBITMQ_HOST"):
-            # send the message along to another queue
-            self.queue_utils.relay_msg('relay_queue', body)
+        # set the slack/log message context
+        context = "ecflow_run_props_callback()"
 
-    def ecflow_rt_run_props_callback(self, channel, method, properties, body):
+        # load the message
+        try:
+            # load the json
+            msg_obj: json = json.loads(body)
+
+            # transform the ecflow messages into the asgs equivalent
+            msg_obj = self.queue_utils.transform_ecflow_to_asgs(msg_obj)
+
+            # get the site id from the name in the message
+            site_id = self.asgs_constants.get_lu_id_from_msg(msg_obj, "physical_location", "site")
+
+            # insure we have a legit location
+            if site_id is None or site_id[0] < 0:
+                # create the error message
+                err_msg = f'{context}: ERROR Unknown physical location {msg_obj.get("physical_location", "")}, Ignoring message'
+
+                # log the event
+                self.logger.error(err_msg)
+
+                # send a message to slack
+                self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+            else:
+                self.logger.debug("site_id: %s", str(site_id))
+
+                # filter out handing - accept runs for all locations, except UCF and George Mason runs for now
+                site_ids = self.asgs_constants.get_site_ids()
+
+                # init the instance id
+                instance_id: int = 0
+
+                # check the site id
+                if site_id[0] in site_ids:
+                    # get the instance id
+                    instance_id = self.asgs_db.get_existing_instance_id(site_id[0], msg_obj)
+
+                    self.logger.info("instance_id: %s", str(instance_id))
+
+                    # we must have an existing instance id
+                    if instance_id > 0:
+                        # get the configuration params
+                        param_list = msg_obj.get("param_list")
+
+                        if param_list is not None:
+                            # insert the records
+                            ret_msg = self.asgs_db.insert_ecflow_config_items(instance_id, param_list, 'ECFLOW')
+
+                            if ret_msg is not None:
+                                err_msg = f'{context}: Error - DB insert for run properties message failed: {ret_msg}, ignoring message.'
+                                self.logger.error(err_msg)
+
+                                # send a message to slack
+                                self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+
+                        else:
+                            err_msg = f"{context}: Error invalid message - 'param_list' key is missing from the run properties message. Ignoring " \
+                                       "message."
+                            self.logger.error(err_msg)
+
+                            # send a message to slack
+                            self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+                    else:
+                        err_msg = f"{context}: Error invalid instance ID. Ignoring message for {msg_obj.get('instance_name', 'N/A')}."
+                        self.logger.error(err_msg)
+
+                        # send a message to slack
+                        self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+                else:
+                    err_msg = f"{context}: Error - Site %s not supported. Ignoring message."
+                    self.logger.error(err_msg)
+
+                    # send a message to slack
+                    self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+        except Exception:
+            err_msg = f"{context}: Error loading the run properties message."
+            self.logger.exception(err_msg)
+
+            # send a message to slack
+            self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+
+    def ecflow_run_time_status_callback(self, channel, method, properties, body):
         """
-        The callback function for the ecflow_rt run properties queue
+        The callback function for the ecflow run time status message queue
 
         :param channel:
         :param method:
@@ -285,7 +374,7 @@ class AsgsQueueCallback:
 
     def hec_ras_callback(self, channel, method, properties, body):
         """
-        The callback function for the HEC/RAS queue
+        The callback function for the HEC/RAS message queue
 
         :param channel:
         :param method:
