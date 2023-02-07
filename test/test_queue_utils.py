9bef733427c1d31119c55c27c2c626d5552a1206
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2023-02-07 10:02:13 -0500

    adding/modifying tests for inserting ecflow run props

--- test/test_queue_utils.py
+++ test/test_queue_utils.py
@@ -13,6 +13,8 @@ import os
 import json
 
 from src.common.queue_utils import QueueUtils
+from src.common.asgs_db import AsgsDb
+from src.common.asgs_constants import AsgsConstants
 
 
 def test_ecflow_transformer():
@@ -23,21 +25,71 @@ def test_ecflow_transformer():
     # load the test json
     with open(os.path.join(os.path.dirname(__file__), 'test_ecflow_run_props.json'), encoding='UTF-8') as test_fh:
         # load the json
-        run_params = json.loads(test_fh.read())
+        run_props = json.loads(test_fh.read())
 
         # instantiate the utility class
         queue_utils = QueueUtils(_queue_name='', _logger=None)
 
         # get the transformed list
-        ret_val = queue_utils.transform_ecflow_to_asgs(run_params)
+        ret_val = queue_utils.transform_ecflow_to_asgs(run_props)
 
-        assert (ret_val == {'suite.uid': '24306857', 'suite.instance_name': 'ncsc_coamps_09L', 'suite.physical_location': 'bridges2.psc.edu',
-                            'suite.project': 'ncfs', 'suite.adcirc.grid': 'ncsc_sab_v1.23', 'time.currentdate': '20220928', 'time.currentcycle': '12',
-                            'forcing.advisory': 'NA', 'forcing.ensemblename': 'coampsforecast', 'forcing.metclass': 'hybrid',
-                            'forcing.stormname': '09L', 'forcing.stormnumber': '09', 'enstorm': 'coampsforecast',
-                            'physical_location': 'bridges2.psc.edu', 'monitoring.rmqmessaging.locationname': 'bridges2.psc.edu',
-                            'instance_name': 'ncsc_coamps_09L', 'instancename': 'ncsc_coamps_09L', 'uid': '24306857', 'ADCIRCgrid': 'ncsc_sab_v1.23',
-                            'adcirc.gridname': 'ncsc_sab_v1.23', 'currentdate': '20220928', 'currentcycle': '12', 'advisory': 'NA',
-                            'asgs.enstorm': 'coampsforecast', 'stormname': '09L', 'forcing.tropicalcyclone.stormname': '09L',
-                            'downloadurl': 'thredds_data/2022/coamps_09L/2022092812/ncsc_sab_v1.23/bridges2.psc.edu/ncsc_coamps_09L',
-                            'output.downloadurl': 'thredds_data/2022/coamps_09L/2022092812/ncsc_sab_v1.23/bridges2.psc.edu/ncsc_coamps_09L'})
+        # add in the expected transformations
+        run_props.update(
+            {'physical_location': 'ht-ncfs.renci.org', 'monitoring.rmqmessaging.locationname': 'ht-ncfs.renci.org', 'instance_name': 'ec95d',
+             'instancename': 'ec95d', 'uid': '90161888', 'ADCIRCgrid': 'ec95d', 'adcirc.gridname': 'ec95d', 'currentdate': '20230206',
+             'currentcycle': '12', 'advisory': 'NA', 'asgs.enstorm': 'gfsforecast', 'enstorm': 'gfsforecast', 'stormname': 'none',
+             'forcing.tropicalcyclone.stormname': 'none', 'config.coupling.waves': '0',
+             'downloadurl': 'https://apsviz-thredds-dev.apps.renci.org/thredds/fileServer/2023/gfs/2023020612/ec95d/ht-ncfs.renci.org/'
+             'ec95d/gfsforecast'})
+
+        # check the result
+        assert (ret_val == run_props)
+
+
+def test_insert_ecflow_config_items():
+    """
+    Tests the parsing of ecflow data
+
+    :return:
+    """
+    # define and init the object used to handle ASGS constant conversions
+    asgs_constants = AsgsConstants(_logger=None)
+
+    # define and init the object that will handle ASGS DB operations
+    asgs_db = AsgsDb(asgs_constants, _logger=None)
+
+    # instantiate the utility class
+    queue_utils = QueueUtils(_queue_name='', _logger=None)
+
+    # load the json
+    with open(os.path.join(os.path.dirname(__file__), 'test_ecflow_run_props.json'), encoding='UTF-8') as test_fh:
+        run_props = json.loads(test_fh.read())
+
+    # transform the ecflow params into addition al asgs params
+    ret_val = queue_utils.transform_ecflow_to_asgs(run_props)
+
+    # add in the expected transformations
+    run_props.update({'physical_location': 'ht-ncfs.renci.org', 'monitoring.rmqmessaging.locationname': 'ht-ncfs.renci.org', 'instance_name': 'ec95d',
+                      'instancename': 'ec95d', 'uid': '90161888', 'ADCIRCgrid': 'ec95d', 'adcirc.gridname': 'ec95d', 'currentdate': '20230206',
+                      'currentcycle': '12', 'advisory': 'NA', 'asgs.enstorm': 'gfsforecast', 'enstorm': 'gfsforecast', 'stormname': 'none',
+                      'forcing.tropicalcyclone.stormname': 'none', 'config.coupling.waves': '0',
+                      'downloadurl': 'https://apsviz-thredds-dev.apps.renci.org/thredds/fileServer/2023/gfs/2023020612/ec95d/ht-ncfs.renci.org/'
+                      'ec95d/gfsforecast'})
+
+    # check the result
+    assert (ret_val == run_props)
+
+    # get the state type id. lets just set this to running for this test run
+    state_id = asgs_constants.get_lu_id('RUNN', "state_type")
+
+    # get the site id
+    site_id = asgs_constants.get_lu_id(run_props.get('suite.physical_location'), 'site')
+
+    # insert a instance record into the DB to get things primed
+    instance_id = asgs_db.insert_instance(state_id, site_id, run_props)
+
+    # insert the run params into the DB
+    ret_val = asgs_db.insert_ecflow_config_items(instance_id, run_props, 'debug')
+
+    # test the result, empty str == success
+    assert (ret_val, '')
