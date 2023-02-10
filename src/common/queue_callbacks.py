f20e64e747e4448b67bf1ba4eebcf533a4706108
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2023-02-10 13:32:25 -0500

    finishing the ecflow run time status msg handler.

--- src/common/queue_callbacks.py
+++ src/common/queue_callbacks.py
@@ -59,7 +59,7 @@ class QueueCallbacks:
 
         self.logger.info("QueueCallback initialization for queue %s complete.", _queue_name)
 
-    def asgs_status_msg_callback(self, channel, method, properties, body):
+    def asgs_status_msg_callback(self, channel, method, properties, body) -> bool:
         """
         main worker that operates on the incoming ASGS status message queue
 
@@ -73,6 +73,9 @@ class QueueCallbacks:
                           properties)
 
         self.logger.debug('Received ASGS status msg %s', body)
+
+        # init the return
+        ret_val = True
 
         context = 'asgs_status_msg_callback()'
 
@@ -149,6 +152,9 @@ class QueueCallbacks:
 
                 # send a message to slack
                 self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+
+                # set the return to indicate failure
+                ret_val = False
         except Exception:
             err_msg = f"{context}: Error loading the ASGS status message."
 
@@ -156,6 +162,12 @@ class QueueCallbacks:
 
             # send a message to slack
             self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+
+            # set the return to indicate failure
+            ret_val = False
+
+        # return the success flag
+        return ret_val
 
     def asgs_run_props_callback(self, channel, method, properties, body):
         """
@@ -234,7 +246,7 @@ class QueueCallbacks:
                             # send a message to slack
                             self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
                     else:
-                        err_msg = f"{context}: Error invalid instance ID. Ignoring message for {msg_obj.get('instance_name', 'N/A')}."
+                        err_msg = f"{context}: Error invalid instance ID. Ignoring message for ASGS {msg_obj.get('physical_location', 'N/A')}."
                         self.logger.error(err_msg)
 
                         # send a message to slack
@@ -330,7 +342,7 @@ class QueueCallbacks:
                             # send a message to slack
                             self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
                     else:
-                        err_msg = f"{context}: Error invalid instance ID. Ignoring message for {msg_obj.get('instance_name', 'N/A')}."
+                        err_msg = f"{context}: Error invalid instance ID. Ignoring message for ECFLOW {msg_obj.get('physical_location', 'N/A')}."
                         self.logger.error(err_msg)
 
                         # send a message to slack
@@ -348,9 +360,12 @@ class QueueCallbacks:
             # send a message to slack
             self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
 
-    def ecflow_run_time_status_callback(self, channel, method, properties, body):
+    def ecflow_run_time_status_callback(self, channel, method, properties, body) -> bool:
         """
-        The callback function for the ecflow run time status message queue
+        The callback function for the ecflow run time status message queue.
+
+        Note: this is a nearly an exact clone of the asgs version above (asgs_status_msg_callback)
+        in case there are any modifications that are needed to comply with the ecflow msg.
 
         :param channel:
         :param method:
@@ -363,10 +378,100 @@ class QueueCallbacks:
 
         self.logger.debug('Received ECFlow_rt msg %s', body)
 
-        # if there is a relay host set send the message over there
-        if os.environ.get("RELAY_RABBITMQ_HOST"):
-            # send the message along to another queue
-            self.queue_utils.relay_msg('relay_queue', body)
+        # init the return
+        ret_val = True
+
+        context = 'ecflow_run_time_status_callback()'
+
+        # load the message
+        try:
+            # load the message
+            msg_obj = json.loads(body)
+
+            # get the site id from the name in the message
+            site_id = self.asgs_constants.get_lu_id_from_msg(msg_obj, "physical_location", "site")
+
+            # get the 3vent type if from the event name in the message
+            event_type_id, event_name = self.asgs_constants.get_lu_id_from_msg(msg_obj, "event_type", "event_type")
+
+            # get the event type if from the event name in the message
+            state_id, state_name = self.asgs_constants.get_lu_id_from_msg(msg_obj, "state", "state_type")
+
+            # get the event advisory data
+            advisory_id = msg_obj.get("advisory_number", "N/A") if (msg_obj.get("advisory_number", "N/A") != "") else "N/A"
+
+            # did we get everything needed
+            if site_id[0] >= 0 and event_type_id >= 0 and state_id >= 0 and advisory_id != 'N/A':
+                # check to see if there are any instances for this site_id yet
+                # this might happen if we start up this process in the middle of a model run
+                instance_id = self.asgs_db.get_existing_instance_id(site_id[0], msg_obj)
+
+                # if this is a STRT event, create a new instance
+                if instance_id < 0 or (event_name == "STRT" and state_name == "RUNN"):
+                    self.logger.debug("create_new_inst is True - creating new inst")
+
+                    # insert the record
+                    instance_id = self.asgs_db.insert_instance(state_id, site_id[0], msg_obj)
+
+                else:  # just update instance
+                    self.logger.debug("create_new_inst is False - updating inst")
+
+                    # update the instance
+                    self.asgs_db.update_instance(state_id, site_id[0], instance_id, msg_obj)
+
+                # check to see if there are any event groups for this site_id and inst yet
+                # this might happen if we start up this process in the middle of a model run
+                event_group_id = self.asgs_db.get_existing_event_group_id(instance_id, advisory_id)
+
+                # if this is the start of a group of Events, create a new event_group record
+                # qualifying group initiation: event type = RSTR
+                # STRT & HIND do not belong to any event group??
+                # For now, it is required that every event belong to an event group, so I will add those as well.
+                # create a new event group if none exist for this site & instance yet or if starting a new cycle
+
+                # +++++++++++++++++++++++++ Figure out how to stop creating a second event group
+                #   after creating first one, when very first RSTR comes for this instance+++++++++++++++++++
+
+                if event_group_id < 0 or (event_name == "RSTR"):
+                    event_group_id = self.asgs_db.insert_event_group(state_id, instance_id, msg_obj)
+                else:
+                    # don't need a new event group
+                    self.logger.debug("Reusing event_group_id: %s", event_group_id)
+
+                    # update event group with this latest state
+                    # added 3/6/19 - will set status to EXIT if this is a FEND or REND event_type
+                    # will hardcode this state id for now, until I get my messaging refactor delivered
+                    if event_name in ['FEND', 'REND']:
+                        state_id = 9
+                        self.logger.debug("Got FEND event type: setting state_id to %s", str(state_id))
+
+                        self.asgs_db.update_event_group(state_id, event_group_id, msg_obj)
+
+                # now insert message into the event table
+                self.asgs_db.insert_event(site_id[0], event_group_id, event_type_id, msg_obj)
+            else:
+                err_msg = f"{context}: Error - Cannot retrieve advisory number, site, event type or state type ids."
+
+                self.logger.error(err_msg)
+
+                # send a message to slack
+                self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+
+                # set the return to indicate failure
+                ret_val = False
+        except Exception:
+            err_msg = f"{context}: Error loading the ECFLOW status message."
+
+            self.logger.exception(err_msg)
+
+            # send a message to slack
+            self.general_utils.send_slack_msg(err_msg, 'slack_issues_channel')
+
+            # set the return to indicate failure
+            ret_val = False
+
+        # return the success flag
+        return ret_val
 
     def hec_ras_callback(self, channel, method, properties, body):
         """
