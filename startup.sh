dad69dfa89050e188ba18ebca514dc2e498e3cde
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-02-01 13:53:47 -0500

    updating to use the new unified database (apsviz + asgs_dashboard -> apsviz) and removing all references to asgs.

--- startup.sh
+++ startup.sh
@@ -1,19 +1,1 @@
-#!/bin/bash
-
-# SPDX-FileCopyrightText: 2022 Renaissance Computing Institute. All rights reserved.
-# SPDX-FileCopyrightText: 2023 Renaissance Computing Institute. All rights reserved.
-# SPDX-FileCopyrightText: 2024 Renaissance Computing Institute. All rights reserved.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-# SPDX-License-Identifier: LicenseRef-RENCI
-# SPDX-License-Identifier: MIT
-
-echo "Sleeping $1 second(s) to give the RabbitMQ time to initialize"
-sleep "$1"
-echo "Starting message handlers..."
-python src/msg_handler/asgs_status_msg_svc.py &
-python src/msg_handler/ecflow_run_props_msg_svc.py &
-python src/msg_handler/ecflow_run_time_msg_svc.py &
-python src/msg_handler/asgs_run_props_msg_svc.py &
-python src/msg_handler/hec_ras_msg_svc.py &
-while true; do sleep 3600; done;
+#!/bin/bash# SPDX-FileCopyrightText: 2022 Renaissance Computing Institute. All rights reserved.# SPDX-FileCopyrightText: 2023 Renaissance Computing Institute. All rights reserved.# SPDX-FileCopyrightText: 2024 Renaissance Computing Institute. All rights reserved.## SPDX-License-Identifier: GPL-3.0-or-later# SPDX-License-Identifier: LicenseRef-RENCI# SPDX-License-Identifier: MITecho "Sleeping $1 second(s) to give the RabbitMQ time to initialize"sleep "$1"echo "Starting message handlers..."#python src/msg_handler/asgs_status_msg_svc.py &#python src/msg_handler/asgs_run_props_msg_svc.py &python src/msg_handler/ecflow_run_props_msg_svc.py &python src/msg_handler/ecflow_run_time_msg_svc.py &python src/msg_handler/hec_ras_msg_svc.py &while true; do sleep 3600; done;
